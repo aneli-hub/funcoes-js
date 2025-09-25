5969802174550c2ee7a04af13560c5b71ed7dedf
aneli-hub <oliveiraaninha587@gmail.com>
2025-09-25 20:54:08 -0300

    alteração

--- Arrays e Objetos/main.js
+++ Arrays e Objetos/main.js
@@ -1,31 +1,43 @@
-let usoDiasDaSemana = [1, 2, 2.4, 5, 1.2, 0.5, 12]; // Domundo, segunda...
-let diasDaSemana = ['domingo', 'segunda', 'Terça', 'Quarta', 'Quinta', 'sexta', 'sabado']
+/**
+Exibir no console as horas de uso para cada dia da semana de maneira formatada,
+adicionando um sinal verde, amarelo ou vermelho, de acordo com um critério seu de horas
+adequadas de uso da rede, por exemplo:
 
+- 0-1 hora por dia = verde
+- 1-3 horas por dia = amarelo
+- 3 horas ou mais por dia = vermelho
+ */
+let usoDiasDaSemana = [2, 2.4, 5, 1.2, 0.5];
+let diasDaSemana = [
+	'Segunda-feira',
+	'Terça-feira  ',
+	'Quarta-feira ',
+	'Quinta-feira ',
+	'Sexta-feira  ',
+];
 
 // Determinar se o uso do dia é verde, amarelo ou vermelho
 const indicadorDeUso = (numeroDeHoras) => {
-if ( numeroDeHoras <= 1 ) {
-        return '❤️';
-  } else if ( numeroDeHoras <= 3 ) {
-        return '💛';
-  } else {
-    return '💚';
-  }
-
+	if ( numeroDeHoras <= 1 ) {
+		return '🟢';
+	} else if ( numeroDeHoras <= 3 ) {
+		return '🟡';
+	} else {
+		return '🔴';
+	}
 }
 
-for( let contador = 0; contador <= 6 ; contador++) {
-     let forol = indicadorDeUso(usoDiasDaSemana[contador]);
-     let dia = diasDaSemana[contador];
-     let horas = usoDiasDaSemana[contador];
-    
-     let mensage = (horas > 1) ? `horas de uso` : `hora de uso`; 
+for(let contador = 0; contador < usoDiasDaSemana.length ; contador++) {
+	let farol = indicadorDeUso(usoDiasDaSemana[contador]);
+	let dia = diasDaSemana[contador];
+	let horas = usoDiasDaSemana[contador];
+	// let mensagem =  'hora de uso';
 
-    console.log(`${farol} 😒 ${dia} | ${horas} | ${mensagem}`);
- }
+	// if ( horas > 1 ) {
+	// 	mensagem = 'horas de uso';
+	// }
 
+	let mensagem = (horas > 1) ? 'horas de uso' : 'hora de uso'; // Operador ternário
 
-// console.log(typeof usoDiasDaSemana);
-// console.log(typeof usoDiasDaSemana[0]);
-// console.log(typeof indicadorDeUso);
-// usoDiasDaSemana[0]
+	console.log(`${farol} 📅 ${dia} | ${horas.toFixed(2)} ${mensagem}`);
+}
--- funcoes/exercicios10/main.js
+++ funcoes/exercicios10/main.js
@@ -1,1 +1,18 @@
 //Escreva uma função que aceita até 3 (três) parâmetros: tipo de forma ("quadrado" ou "retangulo"), e as medidas necessárias. Retorne a área calculada: Quadrado: um lado; Retângulo: largura e altura;
+function calcularArea(tipo, a, b) {
+  if (tipo === "quadrado") {
+    if (typeof a !== "number") return "Valor inválido";
+    return a * a;
+  } 
+  else if (tipo === "retangulo") {
+    if (typeof a !== "number" || typeof b !== "number") return "Valores inválidos";
+    return a * b;
+  } 
+  else {
+    return "Tipo inválido";
+  }
+}
+
+// Exemplo:
+console.log(calcularArea("quadrado", 4));     // 16
+console.log(calcularArea("retangulo", 5, 10)); // 50
