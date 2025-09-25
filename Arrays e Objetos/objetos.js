aff66983b3844d69a1c03add6bda77a21c00623c
aneli-hub <oliveiraaninha587@gmail.com>
2025-09-25 20:23:01 -0300

    correção

--- Arrays e Objetos/objetos.js
+++ Arrays e Objetos/objetos.js
@@ -1,7 +1,12 @@
 let livro = {
-    titulo: 'memorias do do escrivão isaias caminha';
-    autor: ''
-
-
-
+	titulo: 'Memórias do escrivão Isaías Caminha',
+	autor: 'Lima Barreto',
+	ano: 1909,
+	ISBN: '9786599536953',
+	emprestar: (titulo) => {
+		console.log('Você pegou emprestado o livro ' + titulo)
+	}
 }
+
+console.log(typeof livro.emprestar);
+livro.emprestar(livro.titulo)
